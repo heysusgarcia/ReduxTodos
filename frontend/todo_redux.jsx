8ecfa0cb3fc462907007a61009fdbeb450fd6e82
Jesus Garcia <jcgarcia@pandora.com>
2017-06-30 14:44:01 -0700

    added store, tested on console

--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+
+window.store = configureStore;
 
 document.addEventListener("DOMContentLoaded", () => {
   ReactDOM.render(<h1>*</h1>, document.getElementById('content'));
