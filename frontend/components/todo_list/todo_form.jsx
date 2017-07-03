b2773b6dd9c631b0e400536d92b5d8f0444de48e
Jesus Garcia <heysusgarcia@gmail.com>
2017-07-03 00:00:50 -0700

    finished incorporating todo_form component

--- frontend/components/todo_list/todo_form.jsx
+++ frontend/components/todo_list/todo_form.jsx
@@ -1,17 +1,54 @@
+import { uniqueId } from '../../utils/idGenerator';
 import React from 'React';
 
 class TodoForm extends React.Component {
   constructor() {
-    super()
+    super();
     this.state = {
-      title: '',
-      body: ''
+      title: "",
+      body: ""
     };
+
+    this.setTitle = this.setTitle.bind(this);
+    this.setBody = this.setBody.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  setTitle(e) {
+    e.preventDefault();
+    this.setState({title: e.target.value});
+  }
+
+  setBody(e) {
+    e.preventDefault();
+    this.setState({body: e.target.value});
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const id = uniqueId();
+    const { title, body } = this.state;
+
+    const todo = { id, title, body };
+    this.props.receiveTodo(todo);
   }
 
   render() {
+    const { title, body } = this.props;
     return (
-      <h1></h1>
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Title:
+          <input type="text" value={title} onChange={this.setTitle} />
+        </label>
+        <br />
+        <label>
+          Body:
+          <input type="text" value={body} onChange={this.setBody} />
+        </label>
+        <br/>
+        <input type="submit" value="Submit" />
+      </form>
     )
   }
 };
