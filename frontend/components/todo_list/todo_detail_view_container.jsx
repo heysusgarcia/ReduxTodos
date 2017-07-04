26e30235ec984b8df5b7ec4ae9be48d22934f893
Jesus Garcia <heysusgarcia@gmail.com>
2017-07-03 18:23:12 -0700

    added new container

---
+++ frontend/components/todo_list/todo_detail_view_container.jsx
@@ -0,0 +1,6 @@
+import { connect } from 'react-redux';
+import TodoDetailView from './todo_detail_view';
+
+const mapDispatchToProps = (dispatch) => ({
+  removeTodo: (todo) => dispatch(removeTodo(todo))
+});
