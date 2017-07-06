620d1699db62fef27273422065c2ba48ec21d78e
Jesus Garcia <jcgarcia@pandora.com>
2017-07-06 15:32:26 -0700

    fixed typo in validation

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,4 +1,4 @@
 class Todo < ActiveRecord::Base
   validates :string, :text, presence: true
-  validates :done, inclusion: {in [true, false] }
+  validates :done, inclusion: {in: [true, false] }
 end
