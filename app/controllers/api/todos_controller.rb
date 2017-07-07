61fb7a56ea1c0f17609259d4622f5570a4d24ee0
Jesus Garcia <jcgarcia@pandora.com>
2017-07-07 10:32:20 -0700

    added todo_params private method

--- app/controllers/api/todos_controller.rb
+++ app/controllers/api/todos_controller.rb
@@ -1,2 +1,27 @@
 class Api::TodosController < ApplicationController
+
+  def index
+    todos = Todo.all
+    render json: todos
+  end
+
+  def show
+    todo = Todo.find(params[:id])
+    render json: todo
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:title, :body)
+  end
 end
