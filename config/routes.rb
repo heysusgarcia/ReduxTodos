d452fd17a5201b203b5080493e4b97d0d5e698ec
Jesus Garcia <jcgarcia@pandora.com>
2017-07-07 11:21:45 -0700

    created routes namespaced under api and set json as default format

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  namespace :api do
+    resources :todos, only: [:index, :show, :create, :destroy, :update], defaults: { format: 'json' }
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
