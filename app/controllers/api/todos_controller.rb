2cd3f6dd5ce718fb63e926e0acfe94fccf49a316
Jesus Garcia <jcgarcia@pandora.com>
2017-07-07 11:08:23 -0700

    defined show, index, create, update, destroy actions

--- app/controllers/api/todos_controller.rb
+++ app/controllers/api/todos_controller.rb
@@ -1,22 +1,34 @@
 class Api::TodosController < ApplicationController
 
   def index
-    todos = Todo.all
-    render json: todos
+    @todos = Todo.all
+    render json: @todos
   end
 
   def show
-    todo = Todo.find(params[:id])
-    render json: todo
+    @todo = Todo.find(params[:id])
+    render json: @todo
   end
 
   def create
+    @todo = Todo.new(todo_params)
+    if @todo.save
+      render json: @todo
+    else
+      render json: @todo.errors.full_messages, status: 422
   end
 
   def update
+    @todo = Todo.find(params[:id])
+    if @todo.update(todo_params)
+      render json: @todo
+    else
+      render json: @todo.error.full_messages, status: 422
   end
 
   def destroy
+    @todo = Todo.find(params[:id])
+    @todo.destroy
   end
 
   private
