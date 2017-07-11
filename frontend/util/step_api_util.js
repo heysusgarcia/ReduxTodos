05b588a4b82be8e7bdae586ed42342fd8cde83ba
Jesus Garcia <heysusgarcia@gmail.com>
2017-07-10 17:46:01 -0700

    fixed typo in step_api_util URL

--- frontend/util/step_api_util.js
+++ frontend/util/step_api_util.js
@@ -1,14 +1,14 @@
 export const fetchSteps = todo_id => (
   $.ajax({
     method: 'GET',
-    url: `api/todo/${todo_id}/steps`
+    url: `api/todos/${todo_id}/steps`
   })
 )
 
 export const createStep = step => (
   $.ajax({
     method: 'POST',
-    url: `api/todo/${step.todo_id}/steps`,
+    url: `api/todos/${step.todo_id}/steps`,
     data: step
   })
 );
