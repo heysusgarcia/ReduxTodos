a845c63451cc05970e9991fc6bb4f9d23b61f384
Jesus Garcia <jcgarcia@pandora.com>
2017-07-16 13:55:44 -0700

    added user model methods to make it easier to set password_digest column

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ActiveRecord::Base
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
 end
